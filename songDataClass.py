62d76702183c140f5c4e86deebaf97afa5234b9d
Daniel <wodemoneke@gmail.com>
2008-11-06 18:43:48 +0200

    current bugs it loops around track id

--- songDataClass.py
+++ songDataClass.py
@@ -5,7 +5,7 @@ class songData:
     def __init__(self):
         self.songData = []
         self.max = 7 #count of all possible song data
-        self.requiredData = ['Track ID', 'Title', 'Artist', 'Album', 'Track number', 
+        self.requiredData = ['Title', 'Artist', 'Album', 'Track number', 
                              'Duration', 'Use Count']
         
     def _isData(self, data):
@@ -13,13 +13,14 @@ class songData:
         for item in self.requiredData:
             if data.__contains__(item):
                 return True
-            if data.__contains__('Filetype'):
+            if data.__contains__('Track ID'):
+                print 'TRACK ID'
                 self.checkFiletype(data)
         return False
         
     def _cleanData(self, data):
         """Strips unneeded info"""
-        cleanedData = string.split(data, ': ')[1]
+        cleanedData = string.split(data, ': ')[1:-1]
         return cleanedData
     
     def _getKey(self, data):
@@ -37,18 +38,25 @@ class songData:
             pass
             
     def checkFiletype(self, filetype):
-        """if this is called we are at the end of the song.
+        """if this is called we are at the end of the song. OR at the very first song
         check that its a valid type mp3, check if use count exists and if not append"""
+        print 'calling checkFiletype'
         if self.checkIfFull():
             #use count exists
             print 'all accounted for'
             print self.songData
-        else:
+        elif self.checkSongDataCount() == 6:
             self.songData.append(0)
             print 'use count missing'
             print self.songData
+        if self.checkSongDataCount() == 0:
+            print 'data is 0'
+            #Add track ID to the songData array
+            clean = self._cleanData(filetype)
+            self.songData.append(clean)
         #resetdata for next song
-        self.resetValues()
+        else:
+            self.resetValues()
         
         
     def checkIfFull(self):
@@ -64,3 +72,14 @@ class songData:
     
     def resetValues(self):
         self.songData = []
+        
+        
+def run():
+    trackListing = file('./mtp-tracktest2', 'r')
+    sd = songData()
+    for line in trackListing.readlines():
+        print 'Data: ', line
+        sd.newData(line)
+
+if __name__ == "__main__":
+    run()
