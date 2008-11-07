ad0328518ccfc78b9b5df0f596e308af89210736
Daniel <wodemoneke@gmail.com>
2008-11-07 16:39:33 +0200

    cleanup

--- testarea.py
+++
@@ -1,8 +0,0 @@
-import songDataClass
-trackListing = file('./mtp-tracktest2', 'r')
-sd = songDataClass.songData()
-for line in trackListing.readlines():
-    print 'Data: ', line
-    sd.newData(line)
-    
-    
