8450de40d6b6b35081bcb7296fa717f11cc74dd5
Daniel <wodemoneke@gmail.com>
2008-11-07 15:49:07 +0200

    songDataClass complete       prebug test, may still have problems with Album leaf

--- songDataClass.py
+++ songDataClass.py
@@ -5,23 +5,30 @@ class songData:
     def __init__(self):
         self.songData = []
         self.max = 7 #count of all possible song data
-        self.requiredData = ['Title', 'Artist', 'Album', 'Track number', 
+        self.requiredData = ['Track ID', 'Title', 'Artist', 'Album', 'Track number', 
                              'Duration', 'Use Count']
+        self.filetypeReached = False
         
     def _isData(self, data):
         """Returns true if the data submitted is data we want"""
         for item in self.requiredData:
             if data.__contains__(item):
                 return True
-            if data.__contains__('Track ID'):
-                print 'TRACK ID'
-                self.checkFiletype(data)
+        if data.__contains__('Filetype:'):
+            self.filetypeReached = True
+            print self.filetypeReached
+                #self.checkFiletype(data)
+        return False
+        
+    def _isUsecount(self, data):
+        if data.__contains__('Use count'):
+            return True
         return False
         
     def _cleanData(self, data):
         """Strips unneeded info"""
-        cleanedData = string.split(data, ': ')[1:-1]
-        return cleanedData
+        cleanedData = string.split(data, ': ')
+        return cleanedData[1][:-1]
     
     def _getKey(self, data):
         """returns the key (eg. Album) for a string"""
@@ -31,14 +38,26 @@ class songData:
     def newData(self, newData):
         """Needs to find out what data it is and assign 
         it to the correct variable"""
-        if self._isData(newData):
+        if self.filetypeReached == True:
+            print 'is true'
+            #check if this line is usecount if not we need to append
+            if self._isUsecount(newData):
+                self.songData.append(self._cleanData(newData))
+            else:
+                self.songData.append('0 times')
+            self.exportData()
+            self.resetValues()
+            #run newData again, because we have a new track
+            self.newData(newData)
+                
+        elif self._isData(newData):
             clean = self._cleanData(newData)
             self.songData.append(clean)
         else:
             pass
             
     def checkFiletype(self, filetype):
-        """if this is called we are at the end of the song. OR at the very first song
+        """if this is called we are at the end of the song. 
         check that its a valid type mp3, check if use count exists and if not append"""
         print 'calling checkFiletype'
         if self.checkIfFull():
@@ -49,14 +68,9 @@ class songData:
             self.songData.append(0)
             print 'use count missing'
             print self.songData
-        if self.checkSongDataCount() == 0:
-            print 'data is 0'
-            #Add track ID to the songData array
-            clean = self._cleanData(filetype)
-            self.songData.append(clean)
+
         #resetdata for next song
-        else:
-            self.resetValues()
+        self.resetValues()
         
         
     def checkIfFull(self):
@@ -72,13 +86,24 @@ class songData:
     
     def resetValues(self):
         self.songData = []
+        self.filetypeReached = False
         
+    def exportData(self):
+        """Sends song data to database"""
+        #trim seconds and usecount before export
+        self._trimExportData()
+        print self.songData
+        
+    def _trimExportData(self):
+        self.songData[0] = int(self.songData[0])
+        self.songData[4] = int(self.songData[4])
+        self.songData[5] = int(string.split(self.songData[5], ' ')[0]) / 1000
+        self.songData[6] = int(string.split(self.songData[6], ' ')[0])
         
 def run():
-    trackListing = file('./mtp-tracktest2', 'r')
+    trackListing = file('./mtp-tracklisting', 'r')
     sd = songData()
     for line in trackListing.readlines():
-        print 'Data: ', line
         sd.newData(line)
 
 if __name__ == "__main__":
