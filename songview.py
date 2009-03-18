c0879d809a649b9472c96447ad7091b678026141
Daniel Woodhouse <wodemoneke@gmail.com>
2009-03-18 23:21:54 +0200

    moved webservice auth to love section [#41 state:resolved]

--- songview.py
+++ songview.py
@@ -118,7 +118,6 @@ class Songview(object):
 class CacheWindow(Songview):
     def __init__(self, glade_file, db, parent):
         Songview.__init__(self, glade_file, db, parent)
-        self.set_love_auth_state()
         data = self.db.return_unique_scrobbles().fetchall()
         self.fill_liststore(data)
         self.columns = ["Id", "Artist", "Song", "Album", "Rating", "Count"]
@@ -130,42 +129,18 @@ class CacheWindow(Songview):
         self.handlers.update(new_handlers)
         self.wTree.signal_autoconnect(self.handlers)
     
-    def set_love_auth_state(self):
-        if self.parent.session_key:
-            self.wTree.get_widget("main_container").remove(self.wTree.get_widget("auth_area"))
-        else:
-            new_handlers = {
-                "on_love_auth_button_clicked" : self.on_love_auth_button_clicked
-            }
-            self.handlers.update(new_handlers)
+
             
 
     
-    def on_love_auth_button_clicked(self, widget):
-        auth_dialog = self.wTree.get_widget("auth_dialog")
-        #self.wTree.get_widget("auth_ok").set_sensitive(False)
-        webservice = webservices.LastfmWebService()
-        token = webservice.request_session_token()
-        webservice.request_authorisation(token)
-        response = auth_dialog.run()
-        if response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
-            auth_dialog.hide()
-        else:
-            valid, session_key = webservice.create_web_service_session(token)
-            if valid is True:
-                self.parent.usersDB.add_key(self.parent.username, session_key)
-                self.wTree.get_widget("love_auth_state").set_text("Authentication complete")
-                self.wTree.get_widget("love_auth_button").hide()
-                self.parent.session_key = session_key
-            else:
-                self.wTree.get_widget("love_auth_state").set_text(session_key)
-            auth_dialog.hide()
+
     
     
     
 class LovedWindow(Songview):
     def __init__(self, glade_file, db, parent):
         Songview.__init__(self, glade_file, db, parent)
+        self.set_love_auth_state()
         data = self.db.return_pending_love().fetchall()
         self.fill_liststore(data)
         self.append_columns()
@@ -190,7 +165,35 @@ class LovedWindow(Songview):
         else:
             pixbuf = gtk.gdk.pixbuf_new_from_file(path + "love-remove.png")
         return pixbuf
+    
+    def on_love_auth_button_clicked(self, widget):
+        auth_dialog = self.wTree.get_widget("auth_dialog")
+        #self.wTree.get_widget("auth_ok").set_sensitive(False)
+        webservice = webservices.LastfmWebService()
+        token = webservice.request_session_token()
+        webservice.request_authorisation(token)
+        response = auth_dialog.run()
+        if response == gtk.RESPONSE_DELETE_EVENT or response == gtk.RESPONSE_CANCEL:
+            auth_dialog.hide()
+        else:
+            valid, session_key = webservice.create_web_service_session(token)
+            if valid is True:
+                self.parent.usersDB.add_key(self.parent.username, session_key)
+                self.wTree.get_widget("love_auth_state").set_text("Authentication complete")
+                self.wTree.get_widget("love_auth_button").hide()
+                self.parent.session_key = session_key
+            else:
+                self.wTree.get_widget("love_auth_state").set_text(session_key)
+            auth_dialog.hide()
 
+    def set_love_auth_state(self):
+        if self.parent.session_key:
+            self.wTree.get_widget("main_container").remove(self.wTree.get_widget("auth_area"))
+        else:
+            new_handlers = {
+                "on_love_auth_button_clicked" : self.on_love_auth_button_clicked
+            }
+            self.handlers.update(new_handlers)
 
 class BannedWindow(Songview):
     def __init__(self, glade_file, db, parent):
