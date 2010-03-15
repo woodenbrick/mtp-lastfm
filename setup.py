ea93bbdcfd099ab7e23d2d06969a004c7095a6bd
Daniel Woodhouse <wodemoneke@gmail.com>
2010-03-15 14:38:11 +0000

    build c extension during setup

--- setup.py
+++ setup.py
@@ -3,34 +3,34 @@
 from DistUtilsExtra.command import *
 from distutils.core import setup, Extension
 import os
-import glob
+import glob 
 import commands
-
+PROGRAM_NAME = 'mtp-lastfm'
+VERSION = '0.76'
+success = os.system("swig -python cmod/mtpconnect.i")
+print success
 def pkgconfig(*packages, **kw):
     flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
-    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(
-        packages)).split():
+    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
         kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
     return kw
 
-PROGRAM_NAME = 'mtp-lastfm'
-VERSION = '0.76'
-
+mtpconnect_module = Extension('_mtpconnect', sources=['cmod/mtpconnect.c',
+                                                      'cmod/mtpconnect_wrap.c'],
+                           **pkgconfig("libmtp")
+                           )
 glade = glob.glob(os.path.join("glade", "*.glade"))
 images = glob.glob(os.path.join("glade", "*.png"))
 desc = """Scrobble tracks from mtp device to last.fm"""
-long_desc = """The purpose of this program is to scrobble tracks from mtp devices (such as the Creative Zen, or the Zune) to last.fm.  You can Love/Ban tracks before scrobbling, and also use the ratings on your device (5=Love, 1=Ban)."""
-mtpconnect_module = Extension('_mtpconnect',
-                              sources=['mtpconnect.c', 'mtpconnect_wrap.c'],
-                              **pkgconfig("libmtp"))
 
+long_desc = """The purpose of this program is to scrobble tracks from mtp devices (such as the Creative Zen, or the Zune) to last.fm.  You can Love/Ban tracks before scrobbling, and also use the ratings on your device (5=Love, 1=Ban)."""
 setup ( name = PROGRAM_NAME,
         version = VERSION,
         description = desc,
         long_description = long_desc,
         author = 'Daniel Woodhouse',
         author_email = 'wodemoneke@gmail.com',
-        license = 'GPLv3',
+	    license = 'GPLv3',
         platforms = ['Linux'],
         url = 'http://github.com/woodenbrick/mtp-lastfm/tree',
         packages = ['mtplastfm'],
